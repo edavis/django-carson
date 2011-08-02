201ede5a5d6604f9498c336e36122b7a87be9a49
Eric Davis <circa1979@gmail.com>
2011-08-01 19:38:05 -0700

    Skeleton url and view

--- carson/views.py
+++ carson/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from carson.models import *
 
 def index(request):
-    return render(request, "carson/index.html", {})
+    context = {
+        "trusted": Tweet.trusted.all()[:20],
+        "untrusted": Tweet.untrusted.all()[:20],
+    }
+    return render(request, "carson/index.html", context)
