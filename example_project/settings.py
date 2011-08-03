26284d7cf2c90f448c7cf159a3312100ba3f59b4
Eric Davis <ed@npri.org>
2011-08-03 12:22:54 -0700

    Move templates to project
    Carson only provides the "backend" stuff of consuming the Twitter Streaming
    API.  Any and all design choices are left to website designers

--- example_project/settings.py
+++ example_project/settings.py
@@ -109,6 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_ROOT.child("templates"),
 )
 
 INSTALLED_APPS = (
