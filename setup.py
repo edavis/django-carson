81d9c0c0984e5645ef15e5e27e1cfc134ec0b450
Eric Davis <ed@npri.org>
2011-08-08 10:52:47 -0700

    Add URL, author, author email in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-carson",
-    version = "0.1",
-    packages = find_packages(),
+    name         = "django-carson",
+    version      = "0.1",
+    packages     = find_packages(),
+    url          = "https://github.com/edavis/django-carson",
+    author       = "Eric Davis",
+    author_email = "ed@npri.org",
 )
