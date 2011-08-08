b811ec41039aff015fe282ffd8473c1e8bdf76d2
Eric Davis <ed@npri.org>
2011-08-08 12:07:35 -0700

    Back to install_requires instead of requirements.txt
    Don't make the developer jump through more hoops than needed.  Since
    all of these packages are required by carson, install them by default.
    
    Also django-extensions v0.6 had what I needed, so no need to use
    complicated git URL to get it

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ from setuptools import setup, find_packages
 import carson
 
 setup(
-    name         = "django-carson",
-    version      = carson.__version__,
-    packages     = find_packages(exclude=["example_project"]),
-    url          = "https://github.com/edavis/django-carson",
-    author       = "Eric Davis",
-    author_email = "ed@npri.org",
+    name             = "django-carson",
+    version          = carson.__version__,
+    packages         = find_packages(exclude=["example_project"]),
+    url              = "https://github.com/edavis/django-carson",
+    author           = "Eric Davis",
+    author_email     = "ed@npri.org",
+    install_requires = [
+        "pytz==2011h",
+        "oauth2==1.5.170",
+        "httplib2==0.7.1",
+        "django-extensions==0.6",
+    ],
 )
