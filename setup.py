eb527d7ce11407e8bea092550239d5baf14f798e
Eric Davis <ed@npri.org>
2011-08-08 14:10:56 -0700

    Add description, long_description, and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,22 @@ setup(
     url              = "https://github.com/edavis/django-carson",
     author           = "Eric Davis",
     author_email     = "ed@npri.org",
+    description      = "Store tweets posted by a set of users along with tweets containing certain hashtags and/or keywords",
+    long_description = open('README.rst').read(),
     install_requires = [
         "pytz==2011h",
         "oauth2==1.5.170",
         "httplib2==0.7.1",
         "django-extensions==0.6",
+    ],
+    classifiers      = [
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet",
     ],
 )
