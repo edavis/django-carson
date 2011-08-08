9b8c661df4e5c9e192f1fb348c697d640b379ab4
Eric Davis <ed@npri.org>
2011-08-08 11:31:57 -0700

    Exclude example_project

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import carson
 setup(
     name         = "django-carson",
     version      = carson.__version__,
-    packages     = find_packages(),
+    packages     = find_packages(exclude=["example_project"]),
     url          = "https://github.com/edavis/django-carson",
     author       = "Eric Davis",
     author_email = "ed@npri.org",
