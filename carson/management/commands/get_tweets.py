bf06af66d9b10c39a780067b4bed1b41a5692f16
Eric Davis <ed@npri.org>
2011-08-09 09:04:23 -0700

    Bail if there are any empty Twitter IDs

--- carson/management/commands/get_tweets.py
+++ carson/management/commands/get_tweets.py
@@ -15,6 +15,15 @@ class Command(BaseCommand):
         accounts = Account.objects.all()
         tags = Tag.objects.all()
 
+        # Alert when an Account doesn't have its twitter_id set
+        empty_ids = accounts.filter(twitter_id=None)
+        if empty_ids:
+            sys.stderr.write("Missing Twitter IDs for:\n")
+            for account in empty_ids:
+                sys.stderr.write("\t- %s\n" % account.twitter_username)
+            sys.stderr.write("Run ./manage.py lookup_twitter_ids or select \"Lookup Twitter IDs\" in the admin.\n")
+            raise SystemExit
+
         follow = ",".join(map(str, accounts.values_list('twitter_id', flat=True)))
         track = ",".join(tags.values_list('name', flat=True))
 
