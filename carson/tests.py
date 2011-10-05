bb1f89b6cdf16c202f04b65901590aff7c03053f
Eric Davis <ed@npri.org>
2011-10-05 09:17:36 -0700

    Write tests for write_update

--- carson/tests.py
+++ carson/tests.py
@@ -1,6 +1,22 @@
 from django.test import TestCase
 from carson.models import Account
 
+class TwitterUtilsTestCase(TestCase):
+    def test_write_update(self):
+        from carson.utils import write_update
+        from cStringIO import StringIO
+
+        buf = StringIO()
+        write_update("foo", buf)
+        buf.seek(0)
+        self.assertEqual(buf.read(), "\033[2Kfoo\r")
+
+        buf.seek(0)
+        write_update("foo", buf, newline=True)
+        buf.seek(0)
+        self.assertEqual(buf.read(), "\033[2Kfoo\n")
+
+
 class TwitterAccountTestCase(TestCase):
     def test_lookup_twitter_ids(self):
         from carson.utils import lookup_twitter_ids
