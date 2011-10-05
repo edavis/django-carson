fcab2801e5b32e2a24b9f1e1d88e8af66dcbe269
Eric Davis <ed@npri.org>
2011-10-05 09:07:54 -0700

    Put sensitive twitter credentials in twitter_credentials.py

---
+++ example_project/test_settings.py
@@ -0,0 +1,10 @@
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'carson',                      # Or path to database file if using sqlite3.
+    }
+}
+
+from twitter_credentials import *
